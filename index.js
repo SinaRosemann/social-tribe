49cc5e54e0ad4b0602a543f0d4ca71e0c3dc702f
SinaRosemann <sinarosemann.webdesign@gmail.com>
2020-03-19 14:01:58 +0200

    fixed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require("./config");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
